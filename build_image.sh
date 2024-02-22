cea9b3b71d48a15ec93f5ffb89b22f9218cc2b27
Jaeyong Cho <jaeyong.cho.97@gmail.com>
2024-02-22 16:11:04 +0900

    Replace image name

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,3 @@
 cat ~/.ssh/id_rsa.pub > authorized_keys
-docker build --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$GID . -t mpi-test 
+docker build --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$GID . -t docker-mpi
 rm -f authorized_keys
--- create_container.sh
+++ create_container.sh
@@ -1,4 +1,4 @@
 docker network create --subnet=172.20.20.0/16 mpinet
-docker run --name mpi-worker0 -t -d --network mpinet --ip 172.20.20.20 mpi-test
-docker run --name mpi-worker1 -t -d --network mpinet --ip 172.20.20.21 mpi-test 
-docker run --name mpi-worker2 -t -d --network mpinet --ip 172.20.20.22 mpi-test
+docker run --name mpi-worker0 -t -d --network mpinet --ip 172.20.20.20 docker-mpi
+docker run --name mpi-worker1 -t -d --network mpinet --ip 172.20.20.21 docker-mpi 
+docker run --name mpi-worker2 -t -d --network mpinet --ip 172.20.20.22 docker-mpi
